6612899aa15e356c22acff00d4ff5afe835ab6c2
Michael Kornelyuk <6mx9uifzyztm@mail.ru>
2025-04-19 00:19:00 +0300

    review fixes

--- hw10-proxy/src/main/java/ru/proxy/util/TestLoggingProxy.java
+++ hw10-proxy/src/main/java/ru/proxy/util/TestLoggingProxy.java
@@ -1,10 +1,8 @@
 package ru.proxy.util;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import org.slf4j.Logger;
@@ -25,29 +23,27 @@ public class TestLoggingProxy {
                 TestLoggingProxy.class.getClassLoader(), new Class<?>[] {TestLoggingInterface.class}, handler);
     }
 
-    private static Set<String> getLogMethods() {
-        Set<String> result = new HashSet<>();
+    private static Set<Method> getLogMethods() {
+        Set<Method> result = new HashSet<>();
         var methods = TestLogging.class.getDeclaredMethods();
         for (Method method : methods) {
-            for (Annotation annotation : method.getAnnotations()) {
-                if (annotation.annotationType() == Log.class) {
-                    result.add(getMethodDefinition(method));
+            if (method.isAnnotationPresent(Log.class)) {
+                try {
+                    result.add(
+                            TestLoggingInterface.class.getDeclaredMethod(method.getName(), method.getParameterTypes()));
+                } catch (Exception ignored) {
+                    // empty
                 }
             }
         }
         return result;
     }
 
-    private static String getMethodDefinition(Method method) {
-        return method.getName() + "_"
-                + Arrays.stream(method.getParameterTypes()).toList();
-    }
-
     static class TestInvocationHandler implements InvocationHandler {
         private final TestLoggingInterface testLogging;
-        private final Set<String> shouldLogMethods;
+        private final Set<Method> shouldLogMethods;
 
-        TestInvocationHandler(TestLoggingInterface testLogging, Set<String> shouldLogMethods) {
+        TestInvocationHandler(TestLoggingInterface testLogging, Set<Method> shouldLogMethods) {
             this.testLogging = testLogging;
             this.shouldLogMethods = shouldLogMethods;
         }
@@ -55,7 +51,7 @@ public class TestLoggingProxy {
         @SuppressWarnings({"java:S3457", "java:S2629"})
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            if (shouldLogMethods.contains(getMethodDefinition(method))) {
+            if (shouldLogMethods.contains(method)) {
                 log.info("invoking method: {}", method.getName());
                 log.info("parameters: " + "{} ".repeat(args.length), args);
             }
